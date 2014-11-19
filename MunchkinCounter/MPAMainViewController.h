f9da7eaa9a69d1e6325a1b190382e4def74d6b55
NorcalAussie <Matt@Matts-MacBook-Air.local>
2014-11-19 05:00:08 -0800

    First official commit. Main page storyboard complete, coding basic functions next.

---
+++ MunchkinCounter/MPAMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MPAMainViewController.h
+//  MunchkinCounter
+//
+//  Created by Matt Pearce on 11/19/14.
+//  Copyright (c) 2014 MPApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MPAMainViewController : UIViewController
+
+@end
---
+++ MunchkinCounter/MPAMainViewController.m
@@ -0,0 +1,37 @@
+//
+//  MPAMainViewController.m
+//  MunchkinCounter
+//
+//  Created by Matt Pearce on 11/19/14.
+//  Copyright (c) 2014 MPApps. All rights reserved.
+//
+
+#import "MPAMainViewController.h"
+
+@interface MPAMainViewController ()
+
+@end
+
+@implementation MPAMainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
